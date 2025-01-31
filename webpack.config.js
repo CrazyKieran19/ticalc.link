5be8fdbd76c3cde40fc20dfdc9d302321fcf139a
CrazyKieran19 <kfoxmeyer@gmail.com>
2025-01-31 10:09:05 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,40 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'docs'), // Ensure it outputs to docs/
+  mode: 'development', // You might want to change to 'production' when building for release
+
+  entry: {
+    index: './src/index.js', // Entry point for your JavaScript code
   },
+
+  output: {
+    path: path.join(__dirname, 'docs'), // Output directory
+    filename: 'index.js', // Output the JavaScript to index.js
+  },
+
+  watchOptions: {
+    ignored: ['/node_modules/'], // Prevent watching node_modules
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'docs'), // Serve content from docs folder
+      watch: true, // Watch for changes in docs
+    },
+    open: true, // Automatically open the browser when the dev server starts
+    hot: true, // Enable hot reloading (optional, depending on use case)
+  },
+
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.scss$/, // Process SCSS files
+        use: [
+          'style-loader', // Inject styles into the DOM
+          'css-loader', // Turn CSS into CommonJS
+          'sass-loader', // Compile SCSS into CSS
+        ],
       },
     ],
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'docs'),
-    watchContentBase: true,
-    open: true, // Automatically open the HTML page after start
   },
 };
